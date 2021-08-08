186944a18daaeae8fb07157676d8c42ef787a00e
vishnuvardhan860 <vishnuvardhan860@gmail.com>
2021-08-08 16:55:07 +0530

    Added: Adding_Last_Name

--- src/main/java/com/bridgelabz/UserRegistration.java
+++ src/main/java/com/bridgelabz/UserRegistration.java
@@ -4,10 +4,19 @@ import java.util.regex.Pattern;
 
 public class UserRegistration {
 
-        private static final String FIRST_NAME_REGEX = "^[a-zA-Z]+$";
+     // using the pattern for first name
 
-        public boolean validateFirstName(String firstName) {
-            return Pattern.matches(FIRST_NAME_REGEX, firstName);
-        }
+    private static final String FIRST_NAME_REGEX = "^[a-zA-Z]+$";
+
+    public boolean validateFirstName(String firstName) {
+
+        return Pattern.matches(FIRST_NAME_REGEX, firstName);
     }
 
+    private static final String SECOND_NAME_REGEX = "^[a-zA-Z]{3,}$";
+
+    public boolean validateSecondName(String  SecondName) {
+
+        return Pattern.matches(SECOND_NAME_REGEX, SecondName);
+    }
+}
--- src/test/java/com/bridgelabz/UserRegistrationTest.java
+++ src/test/java/com/bridgelabz/UserRegistrationTest.java
@@ -1,28 +1,53 @@
 package com.bridgelabz;
 
 import org.junit.Assert;
+
 import org.junit.Test;
 
 public class UserRegistrationTest {
 
-        @Test
-        public void givenValidFirstName_ShouldReturnTrue_Test() {
+    @Test
+    public void givenValidFirstName_ShouldReturnTrue_Test() {
 
-            UserRegistration userRegistration = new UserRegistration();
+          //  creating the object of userRegistrstion
 
-            boolean result = userRegistration.validateFirstName("VISHNU");
+        UserRegistration userRegistration = new UserRegistration();
 
-            Assert.assertTrue(result);
-        }
+        boolean result = userRegistration.validateFirstName("KVV");
 
-        @Test
-        public void givenInValidFirstName_ShouldReturnFalse_Test() {
-
-            UserRegistration userRegistration = new UserRegistration();
-
-            boolean result = userRegistration.validateFirstName("VISHNU");
-
-            Assert.assertFalse(result);
-        }
+        Assert.assertTrue(result);
     }
 
+    @Test
+    public void givenInValidFirstName_ShouldReturnFalse_Test() {
+
+        UserRegistration userRegistration = new UserRegistration();
+
+        boolean result = userRegistration.validateFirstName("KVV");
+
+        Assert.assertFalse(result);
+
+
+    }
+
+    @Test
+    public void givenValidSecondName_ShouldReturnTrue_Test() {
+
+        UserRegistration userRegistration = new UserRegistration();
+
+        boolean result = userRegistration.validateFirstName("VISHNU");
+
+        Assert.assertTrue(result);
+    }
+
+    @Test
+    public void givenInValidSecondName_ShouldReturnFalse_Test() {
+
+        UserRegistration userRegistration = new UserRegistration();
+
+        boolean result = userRegistration.validateFirstName("VISHNU");
+
+        Assert.assertFalse(result);
+
+    }
+}
